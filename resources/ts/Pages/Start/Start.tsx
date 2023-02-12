1872f3abc1a13767fe01d4c6b50dc09a35485981
Bartosz Woldanski <studio@dziwnykot.pl>
2023-02-12 18:39:08 +0100

    feat: change information on start page
    Add actual information about preparation of query in URL
    with dynamic detected app's location (domain and port).

--- resources/ts/Pages/Start/Start.tsx
+++ resources/ts/Pages/Start/Start.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
-
 const Start = () => {
-
     return (
         <>
-            <div className="h-screen bg-slate-300">
-                <h1 className="text-3xl text-center font-extrabold ">
-                    Hello! Please send query API ../api/weather/country_code/city-name
-                </h1>
+            <div className="h-screen bg-slate-300 ">
+                <div>
+                    <h1 className="text-3xl text-center font-extrabold pt-40">
+                        <span>Hello! This is a weather API.</span>
+                        <br />
+                        <span>Prepare query as below:</span>
+                    </h1>
+                    <h4 className="text-center mt-6">{` ${window.location.hostname}:${window.location.port}/weather-api/[ISO country_code]/[city-name]`}</h4>
+                </div>
             </div>
         </>
     );
