cb02b47e099a56a938e0588f9fc61749f17efbbc
Bartosz Woldanski <studio@dziwnykot.pl>
2023-02-12 18:37:05 +0100

    feat: change route prefix
    From "/api/weather/[country]/[city]"
    to "weather-api/[country]/[city]"

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/weather/index', [ApiWeatherController::class, 'index']);
 // Route::get('/weather/timestamp/{country}/{city}', [ApiWeatherController::class, 'timestamp']);
 // Route::get('/weather/delete/{id}', [ApiWeatherController::class, 'delete']);
-Route::get('/weather/lasttimestamp', [WeatherAPIController::class, 'getLastTimestamp']);
-Route::get('/weather/{country}/{city}', [WeatherAPIController::class, 'manageRequest']);
+// Route::get('/lasttimestamp', [WeatherAPIController::class, 'getLastTimestamp']);
+Route::get('/{country}/{city}', [WeatherAPIController::class, 'manageRequest']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //  return $request->user();
